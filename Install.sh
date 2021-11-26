c2b613102e8a75c64261a7756178d12156670495
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-11-26 19:34:06 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,1 +1,2 @@
 sudo apt autoremove
+sudo apt install git && sudo apt install wget && sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" && sudo apt update && sudo apt upgrade
